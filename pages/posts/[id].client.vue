d23857d961e38d047005611bb29d0ede9afb972a
yungu-2201999 <rguo1022@gmail.com>
2025-03-21 10:28:07 +0800

    update

---
+++ pages/posts/[id].client.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+{{ route.params.id }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+console.log(route.params.id)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
